b46b83c2db086be17f059180599936050b81ae42
sunny <sunny.keila24@gmail.com>
2021-01-16 15:32:55 +0100

    updated

--- src/main/java/com/course/microservice/broker/listener/saga_orchestration/RecordBonusCompensatingListener.java
+++ src/main/java/com/course/microservice/broker/listener/saga_orchestration/RecordBonusCompensatingListener.java
@@ -1,9 +1,8 @@
 package com.course.microservice.broker.listener.saga_orchestration;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
+import com.course.microservice.broker.message.BonusRecordedMessage;
+import com.course.microservice.broker.message.RecordBonusErrorMessage;
+import com.course.microservice.broker.message.RecordBonusMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +11,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
-import com.course.microservice.broker.message.RecordBonusErrorMessage;
-import com.course.microservice.broker.message.BonusRecordedMessage;
-import com.course.microservice.broker.message.RecordBonusMessage;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Component
 public class RecordBonusCompensatingListener {
@@ -27,8 +26,9 @@ public class RecordBonusCompensatingListener {
 	@KafkaListener(topics = "t.saga04.organizationdevelopment.request")
 	@SendTo(value = "t.saga04.organizationdevelopment.response")
 	public BonusRecordedMessage listenRecordBonus(RecordBonusMessage message) throws InterruptedException {
+
 		LOG.debug("[Orchestration-Compensating Saga] Listening record bonus message for appraisal {}",
-				message.getAppraisalId());
+				                                                                     message.getAppraisalId());
 
 		// ...
 		// do business logic here for record bonus message,
@@ -62,7 +62,7 @@ public class RecordBonusCompensatingListener {
 		bonusRecordedMessage.setBonusRecordedDateTime(LocalDateTime.of(LocalDate.of(2021, 06, 30), LocalTime.now()));
 
 		LOG.debug("[Orchestration-Compensating Saga] Publishing to record bonus response topic for appraisal {}",
-				bonusRecordedMessage.getAppraisalId());
+		                                                                        		bonusRecordedMessage.getAppraisalId());
 		return bonusRecordedMessage;
 	}
 
